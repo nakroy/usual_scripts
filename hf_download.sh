8c5dff7e63eae23f72536acdb706ffd1f4e2062b
nakroy <85059179+nakroy@users.noreply.github.com>
2024-07-13 19:39:11 +0800

    Update hf_download.sh

--- hf_download.sh
+++ hf_download.sh
@@ -5,10 +5,14 @@
 # usage explanation: 
 # download model from huggingface: bash hf_download.sh [remote-model-dir] [local-save-dir]
 # download dataset from huggingface: bash hf_download.sh dataset [remote-dataset-dir] [local-save-dir]
+# download model needed token from huggingface: bash hf_download.sh [remote-model-dir] [local-save-dir] [huggingface user access tokens]
 
 # usage example:
 # download Qwen1.5-14B-Chat model from huggingface: bash hf_download.sh Qwen/Qwen1.5-14B-Chat qwen1_5-14b-chat
 # download tatsu-lab/alpaca dataset from huggingface: bash hf_download.sh dataset tatsu-lab/alpaca alpaca
+# download meta-Llama3-Instruct model from huggingface: bash hf_download.sh meta-llama/Meta-Llama-3-8B-Instruct llama3-8b-instruct hf****
+# where hf**** is your huggingface access tokens
+
 
 # use mirror address to speed up for Chinese users
 export HF_ENDPOINT=https://hf-mirror.com
@@ -17,6 +21,14 @@ export HF_ENDPOINT=https://hf-mirror.com
 remote_dir="$1"
 local_dir="$2"
 repo_type="$3"
+token="$4"
+
+# construct token type argument
+if [ "$token" != "" ]; then
+    token_type_arg="--token {$token}"
+else
+    token_type_arg=""
+fi
 
 # construct repo type argument
 if [ "$repo_type" == "dataset" ]; then
@@ -29,7 +41,7 @@ fi
 while true; do
     # read all file names under the local folder, and output the file name as a variable exclude_files
     exclude_files=$(ls "$local_dir" | tr '\n' ' ')
-    if huggingface-cli download --token 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 $repo_type_arg --resume-download "$remote_dir" --local-dir "$local_dir" --exclude $exclude_files --local-dir-use-symlinks False; then
+    if huggingface-cli download $token_type_arg $repo_type_arg --resume-download "$remote_dir" --local-dir "$local_dir" --exclude $exclude_files --local-dir-use-symlinks False; then
         echo "download finish"
         break
     else

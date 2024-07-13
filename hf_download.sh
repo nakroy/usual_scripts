11db8f1eba92c5748d8e85081f58726224af2b75
nakroy <85059179+nakroy@users.noreply.github.com>
2024-07-13 20:12:22 +0800

    Update hf_download.sh

--- hf_download.sh
+++ hf_download.sh
@@ -3,49 +3,111 @@
 # pip install -U huggingface-hub
 
 # usage explanation: 
-# download model from huggingface: bash hf_download.sh [remote-model-dir] [local-save-dir]
-# download dataset from huggingface: bash hf_download.sh [remote-dataset-dir] [local-save-dir] [repo_type]
-# download model needed token from huggingface: bash hf_download.sh [remote-model-dir] [local-save-dir] [huggingface user access tokens]
+# ********************
+# download model from huggingface: 
+# bash hf_download.sh -r [remote-model-dir] -l [local-save-dir]
+# ********************
+# download dataset from huggingface: 
+# bash hf_download.sh -r [remote-dataset-dir] -l [local-save-dir] -y [repo_type]
+# ********************
+# download model needed token from huggingface: 
+# bash hf_download.sh -r [remote-model-dir] -l [local-save-dir] -t [huggingface user access tokens]
+# ********************
+# command usage help
+# bash hf_download.sh -h
+# ********************
 
 # usage example:
-# download Qwen1.5-14B-Chat model from huggingface: bash hf_download.sh Qwen/Qwen1.5-14B-Chat qwen1_5-14b-chat
-# download tatsu-lab/alpaca dataset from huggingface: bash hf_download.sh tatsu-lab/alpaca alpaca dataset
-# download meta-Llama3-Instruct model from huggingface: bash hf_download.sh meta-llama/Meta-Llama-3-8B-Instruct llama3-8b-instruct hf****
+# ********************
+# download Qwen1.5-14B-Chat model from huggingface: 
+# bash hf_download.sh -r Qwen/Qwen1.5-14B-Chat -l qwen1_5-14b-chat
+# ********************
+# download tatsu-lab/alpaca dataset from huggingface: 
+# bash hf_download.sh -r tatsu-lab/alpaca -l alpaca -y dataset
+# ********************
+# download meta-Llama3-Instruct model from huggingface: 
+# bash hf_download.sh -r meta-llama/Meta-Llama-3-8B-Instruct -l llama3-8b-instruct -t hf****
 # where hf**** is your huggingface access tokens
+# ********************
 
 
 # use mirror address to speed up for Chinese users
 export HF_ENDPOINT=https://hf-mirror.com
 
-# get input params
-remote_dir="$1"
-local_dir="$2"
-repo_type="$3"
-token="$4"
+# initialzing arguments
+token=""
+remote_dir=""
+local_dir=""
+repo_type=""
 
-# construct token type argument
-if [ "$token" != "" ]; then
-    token_type_arg="--token {$token}"
+# command usage help
+show_help() {
+    echo "Usage: $0 [-t token] -r remote_dir -l local_dir [-y repo_type]"
+    echo
+    echo "Options:"
+    echo "  -t    Token for authentication (optional)"
+    echo "  -r    Remote directory (required)"
+    echo "  -l    Local directory (required)"
+    echo "  -y    Repository type, e.g., 'dataset' (optional)"
+    echo "  -h    Show this help message"
+}
+
+
+while getopts ":t:r:l:y:h" opt; do
+  case $opt in
+    t) token="$OPTARG"
+    ;;
+    r) remote_dir="$OPTARG"
+    ;;
+    l) local_dir="$OPTARG"
+    ;;
+    y) repo_type="$OPTARG"
+    ;;
+    h) show_help
+       exit 0
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+        show_help
+        exit 1
+    ;;
+  esac
+done
+
+# check required arguments
+if [ -z "$remote_dir" ] || [ -z "$local_dir" ]; then
+  echo "Error: Remote directory (-r) and local directory (-l) are required."
+  show_help
+  exit 1
+fi
+
+# construct token argument
+if [ -n "$token" ]; then
+    token_type_arg="--token $token"
 else
     token_type_arg=""
 fi
 
-# construct repo type argument
+# construct repo_type argument
 if [ "$repo_type" == "dataset" ]; then
     repo_type_arg="--repo-type dataset"
 else
     repo_type_arg=""
 fi
 
-# execute huggingface-cli download command
+#  execute huggingface-cli command
 while true; do
-    # read all file names under the local folder, and output the file name as a variable exclude_files
-    exclude_files=$(ls "$local_dir" | tr '\n' ' ')
+     # read all file names under the local folder, and output the file name as a variable exclude_files
+    if [ -d "$local_dir" ]; then
+        exclude_files=$(ls "$local_dir" | tr '\n' ' ')
+    else
+        exclude_files=""
+    fi
+    
     if huggingface-cli download $token_type_arg $repo_type_arg --resume-download "$remote_dir" --local-dir "$local_dir" --exclude $exclude_files --local-dir-use-symlinks False; then
-        echo "download finish"
+        echo "Download finished"
         break
     else
-        echo "download interrupt, waiting for next retry"
-        sleep 5  # waiting for next retry in 5s
+        echo "Download interrupted, waiting for next retry"
+        sleep 5  # waiting for next retry in 5 seconds
     fi
 done
